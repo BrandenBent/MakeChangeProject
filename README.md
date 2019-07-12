0a2c84e28bc753f7060a0c0e55c040b01920233d
BrandenBent <bent_branden@hotmail.com>
2019-07-12 17:42:23 -0600

    added method to collect payment amount

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 Week 1 homework for Skill Distillery
 Java Bootcamp.
 
+
+This demonstrates basic skills with variables, operators, expressions, and program control constructs.
+
 ### Overview
 
 ### Technologies Used
--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -1,10 +1,21 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class MakeChangeApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static Scanner sc = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		askPay();
+
+		sc.close();
 	}
 
+	public static void askPay() {
+		double cashPaid = 0.0;
+		System.out.println("Enter the amount youre paying. ");
+		cashPaid = sc.nextDouble();
+//		System.out.println(cashPaid);
+	}
 }
