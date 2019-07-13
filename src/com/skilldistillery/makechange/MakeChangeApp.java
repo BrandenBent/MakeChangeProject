6703968072786d2a79a49276201ab91480a8da84
BrandenBent <bent_branden@hotmail.com>
2019-07-13 14:20:07 -0600

    adding working change to bill conversino (0 & 0)

--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -14,9 +14,16 @@ public class MakeChangeApp {
 		cashPaid = askPay();
 		System.out.printf("You hand the cashier $" + cashPaid + "\n");
 		makeChange(cashPaid, itemPrice);
+//		makeCoinage(change);
 
-	sc.close();
+		sc.close();
 	} // END MAIN
+
+	public static double makeCoinage(double change) {
+		System.out.println("test");
+		return change;
+		
+	}
 
 	public static double askPay() {
 		double cashPaid = 0.0;
@@ -39,18 +46,25 @@ public class MakeChangeApp {
 		double change = cashPaid - itemPrice;
 		if (change == 0) {
 			System.out.println("Paid Exact Amount, No Change Provided");
-			
+
 		} else {
 			System.out.println("Your change is " + change);
+
+		}
+		
+		if (change >= 20.00) {
+			int twentyBill = (int)(change/20);
+			System.out.println("Here is " + twentyBill + " twenty dollar bill" +"(" + "s" + ")");
+			change -= twentyBill*20;
 			
 		}
-		return change;
+		if (change >= 10.00) {
+			int tenBill = (int)(change/10);
+			System.out.println("Here is " + tenBill + " ten dollar bill" +"(" + "s" + ")");
+			
+		}
 		
-	}
-
-
-
+		return change;
+	} // END MAKECHANGE();
 
 } // END CLASS
-
-
