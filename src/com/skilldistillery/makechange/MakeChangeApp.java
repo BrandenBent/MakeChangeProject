6a32bf4427bdeef812960a924f233a12114fdaa4
BrandenBent <bent_branden@hotmail.com>
2019-07-13 13:50:48 -0600

    added method to calculate change value

--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -9,16 +9,14 @@ public class MakeChangeApp {
 	public static void main(String[] args) {
 		double cashPaid = 0.0;
 		double itemPrice = 0.0;
-		
+
 		cashPaid = askPay();
 		System.out.printf("You hand the cashier $" + cashPaid + "\n");
 		itemPrice = itemPrice();
-		
-		
-		
-		
-		sc.close();
-	}
+		makeChange(cashPaid, itemPrice);
+
+	sc.close();
+	} // END MAIN
 
 	public static double askPay() {
 		double cashPaid = 0.0;
@@ -27,13 +25,35 @@ public class MakeChangeApp {
 //		System.out.println(cashPaid); <--test code
 		return cashPaid;
 	}
-	
+
 	public static double itemPrice() {
 		double itemPrice = 0.0;
 		System.out.println("Enter the price of the item. ");
 		itemPrice = sc.nextDouble();
 //		System.out.println(); <--test code
 		return itemPrice;
+
+	} // END ITEMPRICE();
+
+	public static double makeChange(double cashPaid, double itemPrice) {
+		double change = cashPaid - itemPrice;
+		
+		if (change == 0) {
+			System.out.println("Paid Exact Amount");
+			
+		} else {
+			System.out.println("Your change is " + change);
+			
+		}
+			
+		
+		return change;
 		
 	}
-}
+
+
+
+
+} // END CLASS
+
+
